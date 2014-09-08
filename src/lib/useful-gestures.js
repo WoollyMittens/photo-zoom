11bc4c251def2d558df53c8d41da9ff0b631d035
Maurice van Creij <maurice.vancreij@webqem.com>
2014-09-09 09:11:20 +1000

    Emulate scaling gesture on Android.

--- src/lib/useful-gestures.js
+++ src/lib/useful-gestures.js
@@ -40,12 +40,19 @@
 			this.obj.addEventListener('mspointermove', this.onChangeTouch());
 			document.body.addEventListener('mspointerup', this.onEndTouch());
 			// set the required events for gestures
-			this.obj.addEventListener('gesturestart', this.onStartGesture());
-			this.obj.addEventListener('gesturechange', this.onChangeGesture());
-			this.obj.addEventListener('gestureend', this.onEndGesture());
-			this.obj.addEventListener('msgesturestart', this.onStartGesture());
-			this.obj.addEventListener('msgesturechange', this.onChangeGesture());
-			this.obj.addEventListener('msgestureend', this.onEndGesture());
+			if ('ongesturestart' in window) {
+				this.obj.addEventListener('gesturestart', this.onStartGesture());
+				this.obj.addEventListener('gesturechange', this.onChangeGesture());
+				this.obj.addEventListener('gestureend', this.onEndGesture());
+			} else if ('msgesturestart' in window) {
+				this.obj.addEventListener('msgesturestart', this.onStartGesture());
+				this.obj.addEventListener('msgesturechange', this.onChangeGesture());
+				this.obj.addEventListener('msgestureend', this.onEndGesture());
+			} else {
+				this.obj.addEventListener('touchstart', this.onStartFallback());
+				this.obj.addEventListener('touchmove', this.onChangeFallback());
+				this.obj.addEventListener('touchend', this.onEndFallback());
+			}
 			// disable the start function so it can't be started twice
 			this.start = function () {};
 		};
@@ -265,6 +272,54 @@
 			// note the start position
 			this.gestureOrigin = null;
 		};
+		// fallback functionality
+		this.startFallback = function (event) {
+			// if the functionality wasn't paused
+			if (!this.paused && event.touches.length === 2) {
+				// note the start position
+				this.gestureOrigin = {
+					'touches' : [
+						{ 'pageX' : event.touches[0].pageX, 'pageY' : event.touches[0].pageY },
+						{ 'pageX' : event.touches[1].pageX, 'pageY' : event.touches[1].pageY }
+					],
+					'target' : event.target || event.srcElement
+				};
+				this.gestureProgression = {
+					'touches' : this.gestureOrigin.touches
+				};
+			}
+		};
+		this.changeFallback = function (event) {
+			// if there is an origin
+			if (this.gestureOrigin && event.touches.length === 2) {
+				// get the coordinates from the event
+				var coords = this.readEvent(event);
+				// calculate the scale factor
+				var scale = 0, progression = this.gestureProgression;
+				scale += (event.touches[0].pageX - event.touches[1].pageX) / (progression.touches[0].pageX - progression.touches[1].pageX);
+				scale += (event.touches[0].pageY - event.touches[1].pageY) / (progression.touches[0].pageY - progression.touches[1].pageY);
+				scale = scale - 2;
+				// get the gesture parameters
+				this.cfg.pinch({
+					'x' : coords.x,
+					'y' : coords.y,
+					'scale' : scale,
+					'event' : event,
+					'target' : this.gestureOrigin.target
+				});
+				// update the current position
+				this.gestureProgression = {
+					'touches' : [
+						{ 'pageX' : event.touches[0].pageX, 'pageY' : event.touches[0].pageY },
+						{ 'pageX' : event.touches[1].pageX, 'pageY' : event.touches[1].pageY }
+					]
+				};
+			}
+		};
+		this.endFallback = function () {
+			// note the start position
+			this.gestureOrigin = null;
+		};
 		// touch events
 		this.onStartTouch = function () {
 			// store the context
@@ -322,8 +377,6 @@
 			var context = this;
 			// return and event handler
 			return function (event) {
-				// get event object
-				event = event || window.event;
 				// optionally cancel the default behaviour
 				context.cancelGesture(event);
 				// handle the event
@@ -336,8 +389,6 @@
 			var context = this;
 			// return and event handler
 			return function (event) {
-				// get event object
-				event = event || window.event;
 				// optionally cancel the default behaviour
 				context.cancelGesture(event);
 				// handle the event
@@ -349,8 +400,39 @@
 			var context = this;
 			// return and event handler
 			return function (event) {
-				// get event object
-				event = event || window.event;
+				// handle the event
+				context.endGesture(event);
+			};
+		};
+		// gesture events
+		this.onStartFallback = function () {
+			// store the context
+			var context = this;
+			// return and event handler
+			return function (event) {
+				// optionally cancel the default behaviour
+				//context.cancelGesture(event);
+				// handle the event
+				context.startFallback(event);
+				context.changeFallback(event);
+			};
+		};
+		this.onChangeFallback = function () {
+			// store the context
+			var context = this;
+			// return and event handler
+			return function (event) {
+				// optionally cancel the default behaviour
+				context.cancelGesture(event);
+				// handle the event
+				context.changeFallback(event);
+			};
+		};
+		this.onEndFallback = function () {
+			// store the context
+			var context = this;
+			// return and event handler
+			return function (event) {
 				// handle the event
 				context.endGesture(event);
 			};
